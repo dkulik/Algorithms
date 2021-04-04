742b20a75a7702599f7a6180ad5d24a2fb118238
dkulik <k.dmytro.s@gmail.com>
2021-04-04 20:04:18 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Algorithms
+![build](https://github.com/dkulik/Algorithms/workflows/build/badge.svg)
+
+common algorithms' implementation.
+type what you need.
+Ex.: Inverse string, count words, ... 
---
+++ source/test1.cxx
@@ -0,0 +1,16 @@
+
+//#include <RetinaDeepLearning.hpp>
+#include <string>
+#include <cassert>
+
+
+int main()
+{
+    //if (a_library_function(0) == "expected result")
+    //{
+        return 0;
+    //}
+    //else 
+    //    return 1;
+}
+
